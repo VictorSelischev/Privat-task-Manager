999249e4ba9369d92f687281642df74cb538b426
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-12-31 16:36:03 +0200

    Add BrowserRouter, PersistGate, Provider

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+// import { store, persistor } from 'redux';
+// import 'modern-normalize';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <BrowserRouter basename="/privat-task-manager/"> */}
+    {/* <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter> */}
     <App />
-    {/* </BrowserRouter> */}
+    {/* </BrowserRouter>
+      </PersistGate>
+    </Provider> */}
   </React.StrictMode>
 );
