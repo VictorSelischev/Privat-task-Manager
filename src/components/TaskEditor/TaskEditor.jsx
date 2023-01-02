a0fc18a7b4c9a9ddb3ad98ebb4657baf959e831e
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2023-01-02 21:32:18 +0200

    handle in components for operations tasksSlice

--- src/components/TaskEditor/TaskEditor.jsx
+++ src/components/TaskEditor/TaskEditor.jsx
@@ -1,10 +1,28 @@
+import { useDispatch } from 'react-redux';
+import { addTask } from 'redux/tasks/tasksOperations';
 import css from './TaskEditor.module.css';
 
 export const TaskEditor = () => {
-    return (
-        <form className={css.form}>
-            <input className={css.input} type="text" name='text' />
-            <button type='submit' className={css.button} >Add task</button>
-        </form>
-    );
-}
+  const dispatch = useDispatch();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const text = form.elements.text.value;
+    if (text !== '') {
+      dispatch(addTask(text));
+      form.reset();
+      return;
+    }
+    alert('Task cannot be empty. Enter some text!');
+  };
+
+  return (
+    <form className={css.form} onSubmit={handleSubmit}>
+      <input className={css.input} type="text" name="text" />
+      <button type="submit" className={css.button}>
+        Add task
+      </button>
+    </form>
+  );
+};
