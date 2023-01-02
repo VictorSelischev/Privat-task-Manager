6a76ff1078c85b7e5dee700dc20cfcf3a40588ec
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2023-01-02 17:10:51 +0200

    store

---
+++ src/redux/store.js
@@ -0,0 +1,17 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+  },
+});
