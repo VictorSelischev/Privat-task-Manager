29bee5f6c2e6738d88a545e4ddb0d33e83fe6b65
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2023-01-03 16:26:55 +0200

    register operation

---
+++ src/redux/auth/authOperations.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.defaults.baseURL = 'https://goit-task-manager.herokuapp.com/';
+
+// Utility to add JWT
+const setAuthHeader = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+// Utility to remove JWT
+const clearAuthHeader = () => {
+  axios.defaults.headers.common.Authorization = '';
+};
+
+/*
+ POST @ /users/signup
+ body: { name, email, password }
+ */
+
+export const register = createAsyncThunk(
+  'auth/register',
+  async (credentials, thunkAPI) => {
+    try {
+      const response = await axios.post('/users/signup', credentials);
+      // After successful registration, add the token to the HTTP header
+      setAuthHeader(response.data.token);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
