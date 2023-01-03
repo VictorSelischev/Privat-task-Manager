035ed73874c28f8a22f480996e7e16c1c06373d6
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2023-01-03 15:09:18 +0200

    Create authSlice.js

---
+++ src/redux/auth/authSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const authInitialState = {
+  user: { name: null, email: null },
+  token: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: authInitialState,
+  extraReducers: {},
+});
+
+export const authReducer = authSlice.reducer;
