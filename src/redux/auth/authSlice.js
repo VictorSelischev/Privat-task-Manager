524fbff78426059ca0d94225bdba4a7e1378a2eb
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2023-01-04 13:25:29 +0200

    extraReducers in authSLice

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -10,7 +10,34 @@ const authInitialState = {
 const authSlice = createSlice({
   name: 'auth',
   initialState: authInitialState,
-  extraReducers: {},
+  extraReducers: {
+    [register.fulfilled](state, action) {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+    },
+    [login.fulfilled](state, action) {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+    },
+    [logOut.fulfilled](state, action) {
+      state.user = { name: null, email: null };
+      state.token = null;
+      state.isLoggedIn = false;
+    },
+    [refreshUser.pending](state) {
+      state.isRefreshing = true;
+    },
+    [refreshUser.fulfilled](state, action) {
+      state.user = action.payload;
+      state.isLoggedIn = true;
+      state.isRefreshing = false;
+    },
+    [refreshUser.rejected](state) {
+      state.isRefreshing = false;
+    },
+  },
 });
 
 export const authReducer = authSlice.reducer;
