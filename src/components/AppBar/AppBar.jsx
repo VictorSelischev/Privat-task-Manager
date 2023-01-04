0eadce07c04e2133987578d61483f519d7a2714f
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2023-01-04 15:35:20 +0200

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,6 +1,7 @@
 import { Navigation } from '../Navigation/Navigation';
 import { useAuth } from 'hooks';
 import { UserMenu } from 'components/UserMenu/UserMenu';
+import { AuthNav } from 'components/AuthNav/AuthNav';
 import css from './AppBar.module.css';
 
 export const AppBar = () => {
